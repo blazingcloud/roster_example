9072d1c7053b4e6051d2a7de9e018274e616ec26
Sarah Allen <sarah@ultrasaurus.com>
2010-10-22 07:26:18 -0400

    roster app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roster::Application.config.secret_token = '2fdb432c509a187129c74de033e9f38daf32c593c0c08f8f14abbf786cd62f59e1758a287a5499b948ec446bef13ef158f36b75a6297090592643bcc30dbc560'
